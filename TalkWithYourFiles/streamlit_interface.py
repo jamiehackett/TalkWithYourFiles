47cd70da4281789556fb70d9a7cd8eb61de9f96a
Jamie Hackett <jami3hack3tt@gmail.com>
2024-04-16 19:09:48 +0100

    changing the layout

--- TalkWithYourFiles/streamlit_interface.py
+++ TalkWithYourFiles/streamlit_interface.py
@@ -29,7 +29,7 @@ def main():
     setup_sidebar(flow_coordinator)
 
     # Setting up each tab for the GUI
-    tab0, tab1, tab2 = st.tabs(["Chat Bot", "QA Chain", "Active Params"])
+    tab0, tab1, tab2 = st.tabs(["Chat Bot", "QA Chain"])
 
 
     with tab0:
@@ -37,8 +37,8 @@ def main():
     with tab1:
         # Tab 1 QA Chain Over Files
         tab1_qa_chain_files(param_controller, flow_coordinator)
-    with tab2:
-        tab2_active_params(param_controller)
+    #with tab2:
+        #tab2_active_params(param_controller)
 
 
 
